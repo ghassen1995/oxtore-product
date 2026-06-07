fe344356fd0e9f0ce145c0871f60c9d430e80f09
Ghassen Kahri <ghassen.kahri@gtt-tunisie.tn>
2026-06-07 17:30:49 +0100

    first commit

---
+++ src/test/java/com/oxtore/product/ProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oxtore.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
