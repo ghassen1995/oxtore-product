abfc553c40c943464db91b60a9fe2453035885a3
Ghassen Kahri <ghassen.kahri@gtt-tunisie.tn>
2026-06-14 13:52:32 +0100

    - Added Product Service interface

--- src/main/java/com/oxtore/product/service/ProductService.java
+++ src/main/java/com/oxtore/product/service/ProductService.java
@@ -1,24 +1,11 @@
 package com.oxtore.product.service;
 
 import com.oxtore.product.entities.Product;
-import com.oxtore.product.repository.ProductRepository;
-import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-@Service
-public class ProductService {
-    private final ProductRepository productRepository;
+public interface ProductService {
+    Product save(Product product);
+    List<Product> findAll();
 
-    public ProductService(ProductRepository productRepository) {
-        this.productRepository = productRepository;
-    }
-
-    public Product save(Product product) {
-        return productRepository.save(product);
-    }
-
-    public List<Product> findAll() {
-        return productRepository.findAll();
-    }
 }
